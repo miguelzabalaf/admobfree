70be7d43cd18d5c0b47973f5b15e056c47fe1e5e
miguelzabalaf <zabalafmiguel@hotmail.com>
2020-05-02 18:50:03 -0500

    He inicializado y configurado admob correctamente para su uso

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig, AdMobFreeRewardVideoConfig } from '@ionic-native/admob-free/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor( private admobFree: AdMobFree ) {}
+
+  showBannerAd() {
+    let bannerConfig: AdMobFreeBannerConfig = {
+        isTesting: true, // Remove in production
+        autoShow: true,//,
+        id: "ca-app-pub-3940256099942544/6300978111"
+    };
+    this.admobFree.banner.config(bannerConfig);
+
+    this.admobFree.banner.prepare().then(() => {
+        // success
+    }).catch(e => alert(e));
+}
+
+showInterstitialAds(){
+  let interstitialConfig: AdMobFreeInterstitialConfig = {
+      isTesting: true, // Remove in production
+      autoShow: true,//,
+      id: "ca-app-pub-3940256099942544/1033173712"
+  };
+  this.admobFree.interstitial.config(interstitialConfig);
+  this.admobFree.interstitial.prepare().then(() => {
+  }).catch(e => alert(e));
+}
+  showRewardVideoAds(){
+      let RewardVideoConfig: AdMobFreeRewardVideoConfig = {
+          isTesting: true, // Remove in production
+          autoShow: true,//,
+          id: "ca-app-pub-3940256099942544/5224354917"
+      };
+      this.admobFree.rewardVideo.config(RewardVideoConfig);
+      this.admobFree.rewardVideo.prepare().then(() => {
+      }).catch(e => alert(e));
+  }
 
 }
